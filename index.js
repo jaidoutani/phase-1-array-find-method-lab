dcda5b96353b888b6d935dabf1e22b64b8d11fc5
Michael Bowlin <jaidoutani@gmail.com>
2022-02-12 23:06:58 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(element => element.result === "W")
+    if (typeof winRecord === "object") {
+        return winRecord.year
+    }
+}
